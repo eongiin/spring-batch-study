a5d85fc10de33d998069d75b1ed9104d3ec4170d
eongiin <eongiin@gmail.com>
2022-09-24 04:33:56 +0900

    feat: JdbcTemplate 이용, rewriteBatchedStatements=true

--- src/main/java/com/example/demo/application/job/CreateArticleJobConfig.java
+++ src/main/java/com/example/demo/application/job/CreateArticleJobConfig.java
@@ -11,14 +11,16 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.data.RepositoryItemWriter;
-import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @Slf4j
@@ -27,6 +29,7 @@ public class CreateArticleJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final ArticleRepository articleRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @Bean
     public Job createArticleJob() {
@@ -39,7 +42,7 @@ public class CreateArticleJobConfig {
     @Bean
     public Step createArticleStep() {
         return stepBuilderFactory.get("createArticleStep")
-                .<ArticleModel, Article>chunk(100)
+                .<ArticleModel, Article>chunk(1000)
                 .reader(createArticleReader())
                 .processor(createArticleProcessor())
                 .writer(createArticleWriter())
@@ -66,9 +69,11 @@ public class CreateArticleJobConfig {
      */
     @Bean
     public ItemProcessor<ArticleModel, Article> createArticleProcessor() {
+        LocalDateTime now = LocalDateTime.now();
         return articleModel -> Article.builder()
                 .title(articleModel.getTitle())
                 .content(articleModel.getContent())
+                .createdAt(now)
                 .build();
     }
 
@@ -77,9 +82,14 @@ public class CreateArticleJobConfig {
      * entityManagerFactory나 Repository와 같은 JPA 관련된 클래스여야지만 사용 가능
      */
     @Bean
-    public RepositoryItemWriter<Article> createArticleWriter() {
-        return new RepositoryItemWriterBuilder<Article>()
-                .repository(articleRepository)
-                .build();
+    public ItemWriter<Article> createArticleWriter() {
+        return articles -> jdbcTemplate.batchUpdate("insert into Article (title, content, createdAt) values (?, ?, ?)",
+                articles,
+                1000,
+                (ps, article) -> {
+                    ps.setObject(1, article.getTitle());
+                    ps.setObject(2, article.getContent());
+                    ps.setObject(3, article.getCreatedAt());
+                });
     }
 }
