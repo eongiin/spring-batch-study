aaf50f7d655243d44145e345e66686df3e8075e1
eongiin <eongiin@gmail.com>
2022-09-25 18:52:27 +0900

    feat: batch, demo 스키마 분리

--- src/main/java/com/example/demo/application/job/CreateArticleJobConfig.java
+++ src/main/java/com/example/demo/application/job/CreateArticleJobConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.application.job;
 import com.example.demo.application.job.param.CreateArticleJobParam;
 import com.example.demo.application.model.ArticleModel;
 import com.example.demo.domain.entity.Article;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -11,32 +10,49 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
 
 @Configuration
 @Slf4j
-@RequiredArgsConstructor
 public class CreateArticleJobConfig {
+
+    private static final int CHUNK_SIZE = 1000;
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final CreateArticleJobParam createArticleJobParam;
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate demoJdbcTemplate;
+    private final EntityManagerFactory demoEntityManagerFactory;
+
+    public CreateArticleJobConfig(JobBuilderFactory jobBuilderFactory,
+                                  StepBuilderFactory stepBuilderFactory,
+                                  CreateArticleJobParam createArticleJobParam,
+                                  @Qualifier("demoJdbcTemplate") JdbcTemplate demoJdbcTemplate,
+                                  @Qualifier("demoEntityManagerFactory") EntityManagerFactory demoEntityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.createArticleJobParam = createArticleJobParam;
+        this.demoJdbcTemplate = demoJdbcTemplate;
+        this.demoEntityManagerFactory = demoEntityManagerFactory;
+    }
+
 
     @Bean
     public Job createArticleJob() {
         return jobBuilderFactory.get("createArticleJob")
-                .incrementer(new RunIdIncrementer())
                 .start(createArticleStep())
                 .build();
     }
@@ -45,23 +61,22 @@ public class CreateArticleJobConfig {
     @JobScope
     public Step createArticleStep() {
         return stepBuilderFactory.get("createArticleStep")
-                .<ArticleModel, Article>chunk(1000)
+                .<ArticleModel, Article>chunk(CHUNK_SIZE)
                 .reader(createArticleReader())
                 .processor(createArticleProcessor())
-                .writer(createArticleWriter())
+                .writer(writer())
+                .faultTolerant()
+                .skipLimit(3)
+                .skip(FlatFileParseException.class)
                 .build();
     }
 
-    /**
-     * Articles.csv 파일 읽기
-     */
     @Bean
     @StepScope
     public FlatFileItemReader<ArticleModel> createArticleReader() {
-        log.info("PARAM!!!!!!!!!!! {}", createArticleJobParam.getName());
         return new FlatFileItemReaderBuilder<ArticleModel>()
                 .name("createArticleReader")
-                .resource(new ClassPathResource("Articles.csv"))
+                .resource(new ClassPathResource(createArticleJobParam.getName()))
                 .delimited()
                 .names("title", "content")
                 .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
@@ -69,9 +84,6 @@ public class CreateArticleJobConfig {
                 .build();
     }
 
-    /**
-     * ArticleModel -> Article
-     */
     @Bean
     public ItemProcessor<ArticleModel, Article> createArticleProcessor() {
         LocalDateTime now = LocalDateTime.now();
@@ -82,19 +94,28 @@ public class CreateArticleJobConfig {
                 .build();
     }
 
-    /**
-     * 데이터 db에 저장
-     * entityManagerFactory나 Repository와 같은 JPA 관련된 클래스여야지만 사용 가능
-     */
     @Bean
     public ItemWriter<Article> createArticleWriter() {
-        return articles -> jdbcTemplate.batchUpdate("insert into Article (title, content, createdAt) values (?, ?, ?)",
+        return articles -> demoJdbcTemplate.batchUpdate("insert into Article (title, content, createdAt) values (?, ?, ?)",
                 articles,
-                1000,
+                CHUNK_SIZE,
                 (ps, article) -> {
                     ps.setObject(1, article.getTitle());
                     ps.setObject(2, article.getContent());
                     ps.setObject(3, article.getCreatedAt());
                 });
+    }
+
+    /*    @Bean
+        public ItemWriter<Article> writer() {
+            return new JdbcBatchItemWriterBuilder<Article>()
+                    .dataSource(this.demoDataSource)
+                    .build();
+        }*/
+    @Bean
+    public ItemWriter<Article> writer() {
+        return new JpaItemWriterBuilder<Article>()
+                .entityManagerFactory(this.demoEntityManagerFactory)
+                .build();
     }
 }
