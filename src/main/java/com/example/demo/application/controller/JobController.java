81b7883f76d10a893474e4f4251c0b630ac06603
eongiin <eongiin@gmail.com>
2022-09-26 23:29:09 +0900

    refactor: Service 호출하는 것으로 변경

--- src/main/java/com/example/demo/application/controller/JobController.java
+++ src/main/java/com/example/demo/application/controller/JobController.java
@@ -1,27 +1,19 @@
 package com.example.demo.application.controller;
 
+import com.example.demo.service.JobService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("jobs")
 @RequiredArgsConstructor
 public class JobController {
-
-    private final Job createArticleJob;
-    private final JobLauncher jobLauncher;
+    private final JobService jobService;
 
     @GetMapping("/create-articles")
     public void runCreateArticleJob() throws Exception {
-        jobLauncher.run(createArticleJob, new JobParametersBuilder()
-                .addDate("date", new Date())
-                .toJobParameters());
+        jobService.runCreateArticleJob();
     }
 }
