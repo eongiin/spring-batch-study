80e4c84461e624fac62285c6fc668724e64ac7a2
eongiin <eongiin@gmail.com>
2022-09-26 19:52:02 +0900

    chore: 오타 수정

--- src/main/java/com/example/demo/domain/repository/BoardRepository.java
+++ src/main/java/com/example/demo/domain/repository/BoardRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.domain.repository;
 
-import com.example.demo.domain.entity.Article;
+import com.example.demo.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Article, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
