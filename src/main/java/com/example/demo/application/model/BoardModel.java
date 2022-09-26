fa1168f49b8b9f9913aba599d6c03f48d5b7e91d
eongiin <eongiin@gmail.com>
2022-09-26 18:14:50 +0900

    feat: 병렬처리로 여러 파일에 있는 정보 읽어서 DB에 저장하기
    만 개씩 데이터가 들어 있는 파일 40개를 병렬처리로 읽어서 Board 테이블에 저장하는 작업을 진행했습니다

---
+++ src/main/java/com/example/demo/application/model/BoardModel.java
@@ -0,0 +1,11 @@
+package com.example.demo.application.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardModel {
+    private String title;
+    private String content;
+}
