81fc009192afb797996a2178ded8195471765909
eongiin <eongiin@gmail.com>
2022-09-27 23:16:00 +0900

    feat: 오래된 데이터 hard delete
    step1 - 삭제할 데이터 DeletedBoard에 백업
    step2 - 삭제

--- src/main/java/com/example/demo/domain/repository/BoardRepository.java
+++ src/main/java/com/example/demo/domain/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -11,4 +12,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     Page<Board> findAllByIdBetween(long mindId, long maxId, Pageable pageable);
 
     Page<Board> findAllByCreatedAtBefore(LocalDateTime createdAt, Pageable pageable);
+
+    @Transactional
+    void deleteAllByIdIn(Iterable<Long> ids);
 }
